b2f210305b7f21bf852f8a28a501d1f436f0b221
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-01 01:49:42 +0530

    removed college website and color switch btn

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// background color change
-function changeBG(color) {
-    document.body.style.backgroundColor = color;
-    let txtChng = document.getElementById("txt1")
-    console.log(txtChng)
-    if (color == "#000000") {
-        txtChng.style.color = "#ffffff";
-    } else {
-        txtChng.style.color = "#000000";
-    }
-}
 // sticky navbar
 window.addEventListener('scroll', function () {
     let navbar = document.getElementById("stickyNavbar");
