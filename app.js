edcd32b98283ea47e50ac4879095572050569629
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-04 00:43:17 +0530

    sticky navbar removed

--- app.js
+++
@@ -1,9 +0,0 @@
-// sticky navbar
-window.addEventListener('scroll', function () {
-    let navbar = document.getElementById("stickyNavbar");
-    if (window.pageYOffset >= 183) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky")
-    }
-});
